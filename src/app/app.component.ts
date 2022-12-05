81c4386278dcd94231deb346ac03b2e8d5d929f0
dymafr <32801166+dymafr@users.noreply.github.com>
2022-12-05 14:50:37 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
-import { MatPaginator } from "@angular/material/paginator";
-import { MatTableDataSource } from "@angular/material/table";
-import { User } from "./shared/interfaces/user.interface";
-import { UserService } from "./shared/services/user.service";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { User } from './shared/interfaces/user.interface';
+import { UserService } from './shared/services/user.service';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   public dataSource: MatTableDataSource<User> = new MatTableDataSource();
-  public displayedColumns = ["gender", "cell", "email", "nat", "phone"];
-  @ViewChild(MatPaginator) paginateur: MatPaginator;
+  public displayedColumns = ['gender', 'cell', 'email', 'nat', 'phone'];
+  @ViewChild(MatPaginator) paginateur!: MatPaginator;
 
   constructor(private userService: UserService) {}
 
